b992d2ec4a0f44f7c0812ce8df4fed022bcbb382
Akil0007 <akilkumar@fourbends.com>
2023-07-25 17:07:32 +0530

    Updated scratch changes.

---
+++ third_party/python/_next/static/chunks/pages/Editor/python-c187dbb7e450617d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[217],{3530:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/Editor/python",function(){return u(8089)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=3530)}),_N_E=n.O()}]);
---
+++ third_party/python/index.php
@@ -0,0 +1,10 @@
+<?php
+require('../../config.php');
+$PAGE->set_pagelayout('thirdparty');
+echo $OUTPUT->header();
+?>
+
+
+
+<?php
+echo $OUTPUT->footer();
