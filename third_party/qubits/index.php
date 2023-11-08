cefe99928a84a46e9b34860ff77f2d05b70d9010
sivakumarrfourbends <109653892+sivakumarrfourbends@users.noreply.github.com>
2023-11-08 17:06:18 +0530

    Assignment editremove button diasble (#214)
    * edit-remove-disable-update
    
    * teacher--student-view-update--buildfiles
    
    ---------
    
    Co-authored-by: kameshfourbends <31654071+kameshfourbends@users.noreply.github.com>

--- third_party/qubits/index.php
+++ third_party/qubits/index.php
@@ -2,46 +2,46 @@
 require('../../config.php');
 require_login();
 $elanguage      = optional_param('language', '', PARAM_TEXT);
-$clsname = 'page-editor-qubits-'.$elanguage;
+$clsname = 'page-editor-qubits-' . $elanguage;
 $PAGE->set_pagelayout('thirdparty');
 $PAGE->add_body_class($clsname);
 echo $OUTPUT->header();
 ?>
 <div id="__next"></div>
-<?php if($elanguage=="webeditor") { ?>
-<script id="__NEXT_DATA__" type="application/json">
-	{
-	"props": {
-		"pageProps": {}
-	},
-	"page": "/CloudIDE",
-	"query": {},
-	"buildId": "Uax2aoFve0fGjmf5qSMjb",
-	"nextExport": true,
-	"autoExport": true,
-	"isFallback": false,
-	"scriptLoader": []
-	}
-</script>
+<?php if ($elanguage == "webeditor") { ?>
+	<script id="__NEXT_DATA__" type="application/json">
+		{
+			"props": {
+				"pageProps": {}
+			},
+			"page": "/CloudIDE",
+			"query": {},
+			"buildId": "A1dhyo6H_joRa_TtvJg7a",
+			"nextExport": true,
+			"autoExport": true,
+			"isFallback": false,
+			"scriptLoader": []
+		}
+	</script>
 <?php
 } else { ?>
-<script id="__NEXT_DATA__" type="application/json">
-	{
-		"props": {
-			"pageProps": {},
-			"__N_SSG": true
-		},
-		"page": "/Editor/[language]",
-	"query": {
-	  "language": "<?php echo $elanguage; ?>"
-	},
+	<script id="__NEXT_DATA__" type="application/json">
+		{
+			"props": {
+				"pageProps": {},
+				"__N_SSG": true
+			},
+			"page": "/Editor/[language]",
+			"query": {
+				"language": "<?php echo $elanguage; ?>"
+			},
 
-		"buildId": "Uax2aoFve0fGjmf5qSMjb",
-		"isFallback": false,
-		"gsp": true,
-		"scriptLoader": []
-	}
-</script>
+			"buildId": "A1dhyo6H_joRa_TtvJg7a",
+			"isFallback": false,
+			"gsp": true,
+			"scriptLoader": []
+		}
+	</script>
 <?php }
 echo $OUTPUT->footer();
 
