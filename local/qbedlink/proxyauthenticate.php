476cdd3522e50a9b8a795c8b745971faf72b7426
Kamesh V <kamesh.veerachamy@fourbends.com>
2023-11-27 15:59:28 -0500

    Updated latest changes based on token authentication

--- local/qbedlink/proxyauthenticate.php
+++ local/qbedlink/proxyauthenticate.php
@@ -7,6 +7,13 @@ global $CFG, $DB, $USER, $OUTPUT;
 // http://qubits.localhost.com/local/qbedlink/proxyauthenticate.php?wstoken=...&wsfunction=local_qbedlink_auth_edlinkuser&moodlewsrestformat=json
 
 $email = required_param('email', PARAM_TEXT);
+$token = required_param('token', PARAM_TEXT);
+//d194573de478fa1a83a8b0079a9df01b
+$token = str_replace("869o", "", $token);
+$rtoken = $DB->get_record("external_tokens", array("id" => 2));
+if($rtoken->token!==$token){
+    throw new moodle_exception('Invalid Token', 'error');
+}
 
 $auser = $DB->get_record("user",[
     "email" => $email
@@ -16,33 +23,18 @@ $rst = null;
 
 if($auser){
     if($auser->auth != "edlink")
-      $errmsg = "Current User Authentication type is not edlink";
+      throw new moodle_exception('Current User Authentication type is not edlink', 'error');
 
     if($auser->suspended)
-       $errmsg = "Current User has been suspended";
+      throw new moodle_exception('Current User has been suspended', 'error');
 
     if($auser->deleted)
-       $errmsg = "Current User is not available";
-    
-    if(empty($errmsg)){
-        $username = $auser->username;
-        $user = get_complete_user_data('username', $username);
-        $curesult = complete_user_login($user);
-        //set_moodle_cookie($user->username);
-        //$url = $CFG->wwwroot.'/my/courses.php';
-        //redirect($url);
-        echo "Set Cookie <br/>";
-        echo "<pre>"; print_r($curesult); echo "</pre>"; exit;
-        /* if($curesult){
-            $rst = ["result" => true];
-        }else{
-            $rst = ["result" => false, "validationerror" => "Authentication raised some issues"];
-        } */
-    }else{
-       // $rst = ["result" => false, "validationerror" => $errmsg];
-    }
-}else{
-   // $rst = ["result" => false, "validationerror" => "Given user is not available"];
-}
+      throw new moodle_exception('Current User is not available', 'error');
 
-//echo json_encode($rst);
+    $username = $auser->username;
+    $user = get_complete_user_data('username', $username);
+    $curesult = complete_user_login($user);
+    $url = $CFG->wwwroot.'/my/courses.php';
+    redirect($url);
+    exit;
+}
