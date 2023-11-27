2a5398522719e97e0e48c814f95644613f2a492e
kameshfourbends <31654071+kameshfourbends@users.noreply.github.com>
2023-11-27 16:01:24 -0500

    Feature edlink auth (#237)
    * Completed the Edlink Authentication
    
    * Interim commit for Edlink authentication
    
    * Updated latest changes based on token authentication

---
+++ local/qbedlink/proxyauthenticate.php
@@ -0,0 +1,40 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+require_once('../../config.php');
+require_once($CFG->libdir . '/adminlib.php');
+require_once($CFG->dirroot.'/user/lib.php');
+global $CFG, $DB, $USER, $OUTPUT;
+// http://qubits.localhost.com/local/qbedlink/proxyauthenticate.php?wstoken=...&wsfunction=local_qbedlink_auth_edlinkuser&moodlewsrestformat=json
+
+$email = required_param('email', PARAM_TEXT);
+$token = required_param('token', PARAM_TEXT);
+//d194573de478fa1a83a8b0079a9df01b
+$token = str_replace("869o", "", $token);
+$rtoken = $DB->get_record("external_tokens", array("id" => 2));
+if($rtoken->token!==$token){
+    throw new moodle_exception('Invalid Token', 'error');
+}
+
+$auser = $DB->get_record("user",[
+    "email" => $email
+]);
+$errmsg = null;
+$rst = null;
+
+if($auser){
+    if($auser->auth != "edlink")
+      throw new moodle_exception('Current User Authentication type is not edlink', 'error');
+
+    if($auser->suspended)
+      throw new moodle_exception('Current User has been suspended', 'error');
+
+    if($auser->deleted)
+      throw new moodle_exception('Current User is not available', 'error');
+
+    $username = $auser->username;
+    $user = get_complete_user_data('username', $username);
+    $curesult = complete_user_login($user);
+    $url = $CFG->wwwroot.'/my/courses.php';
+    redirect($url);
+    exit;
+}
