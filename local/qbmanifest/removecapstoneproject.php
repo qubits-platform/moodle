468ef0d34dfb932c2e50845b61bcd7b9226b3674
Kamesh V <kamesh.veerachamy@fourbends.com>
2024-01-03 16:55:40 +0530

    Added code for removed capstone project sections and page module from courses.

--- local/qbmanifest/removecapstoneproject.php
+++ local/qbmanifest/removecapstoneproject.php
@@ -2,6 +2,8 @@
 require_once('../../config.php');
 require_once($CFG->libdir . '/adminlib.php');
 require_once($CFG->libdir.'/csvlib.class.php');
+require_once($CFG->dirroot . '/course/lib.php');
+require_once($CFG->dirroot . '/course/modlib.php');
 global $CFG, $DB;
 
 $iid = csv_import_reader::get_new_iid('capstoneproject');
@@ -19,19 +21,57 @@ while ($row = $cir->next()) {
     echo "</pre>";
     // get_course_sections($row[0]);
     // course_delete_section_async
+    delete_course_modules($row[1]);
+    delete_course_sections($row[0]);
 }
 
-function get_course_sections($uid){
+function delete_course_sections($uid){
     global $DB;
     $likeuid = $DB->sql_like('uid', ':uid');
     $uid = $uid.'%';
     $csections = $DB->get_records_sql(
-        "SELECT id FROM {course_sections} WHERE {$likeuid}",
+        "SELECT * FROM {course_sections} WHERE {$likeuid}",
         [
             'uid' => $uid,
         ]
     );
-    return $csections;
+
+    foreach($csections as $csection){
+        $scourse = $DB->get_record("course", [
+           "id" => $csection->course
+        ]);
+        echo "<pre>";
+        print_r($csection);
+        echo "</pre>";
+        course_delete_section($scourse, $csection);
+    }
+}
+
+function delete_course_modules($uid){
+    global $DB;
+    $likeuid = $DB->sql_like('uid', ':uid');
+    $uid = $uid.'%';
+    $course_modules = $DB->get_records_sql(
+        "SELECT id,course FROM {qubitspage} WHERE {$likeuid}",
+        [
+            'uid' => $uid,
+        ]
+    );
+    
+    foreach($course_modules as $course_module){
+        $instance = $course_module->id;
+        $course_id = $course_module->course;
+        $course_mdl = $DB->get_record("course_modules",[
+            "course" => $course_id,
+            "instance" => $instance,
+            "module" => 19
+        ]);
+        echo "<pre>";
+        print_r($course_mdl);
+        echo "</pre>";
+        course_delete_module($course_mdl->id);
+    }
+
 }
 
 
